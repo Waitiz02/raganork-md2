db071f26136984e0adc1cee257c43dd8ecceaea0
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-12-13 19:43:45 +0530

    fix

--- plugins/misc/yt.js
+++ plugins/misc/yt.js
@@ -15,8 +15,12 @@ return audioTo;
 async function downloadYT(vid,type = 'video',quality = '360p'){
  try { 
 var result = (await require("axios")(`https://y2mate.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
+if (!result.url) result = (await require("axios")(`https://y2mate.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
+if (!result.url) result = (await require("axios")(`https://y2mate.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
 } catch {
 var result = (await require("axios")(`https://y2mate-2.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
+if (!result.url) result = (await require("axios")(`https://y2mate-2.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
+if (!result.url) result = (await require("axios")(`https://y2mate-2.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
 } 
 return result 
 }
