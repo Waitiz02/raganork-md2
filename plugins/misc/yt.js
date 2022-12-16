562ceeed51ef4b56eb8c39f84d983ecee8995358
Sourav <87891508+souravkl11@users.noreply.github.com>
2022-12-16 23:11:38 +0530

    rewrite functions

--- plugins/misc/yt.js
+++ plugins/misc/yt.js
@@ -1,16 +1,22 @@
 let fetch = require('node-fetch')
 const axios = require("axios")
 let { JSDOM } = require('jsdom')
-const Innertube = require('youtubei.js');
 
 async function dlSong(vid){
-const yt = await new Innertube({ gl: 'US' });
-const audinfo = await yt.getStreamingData(vid, {type:"audio"});
-let audioUrl = audinfo.selected_format.url
-let abuffer = await require("raganork-bot").skbuffer(audioUrl)
-let audioTo = "./temp/song.mp3"
-await require("fs").writeFileSync(audioTo, abuffer)
-return audioTo;
+  const { Innertube, UniversalCache } = require('youtubei.js');
+  const { readFileSync, existsSync, mkdirSync, createWriteStream } = require('fs');
+  const {streamToIterable} = require('youtubei.js/dist/src/utils/Utils');
+  const yt = await Innertube.create({ cache: new UniversalCache() });
+  const stream = await yt.download(vid, {
+    type: 'audio', 
+    quality: 'best',
+    format: 'mp4'
+  });
+  const file = createWriteStream(`./temp/song.m4a`);
+  for await (const chunk of streamToIterable(stream)) {
+    file.write(chunk);
+  }
+  return true;
 }
 
 async function downloadYT(vid,type = 'video',quality = '360p'){
@@ -18,8 +24,7 @@ async function downloadYT(vid,type = 'video',quality = '360p'){
 var result = (await axios(`https://y2mate.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
 if (!result.url) result = (await axios(`https://y2mate.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
 } catch {
-var result = (await axios(`https://y2mate-2.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
-if (!result.url) result = (await axios(`https://y2mate-2.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
+var result = (await axios(`https://y2mate.souravkl11.xyz/get?vid=${vid}&type=${type}&resolution=${quality}`)).data
 } 
 return result 
 }
