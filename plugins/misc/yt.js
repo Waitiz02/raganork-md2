7d3ed40cd0b26da58fd9288b6ce282ab13fff25c
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-01-10 19:45:41 +0530

    x

--- plugins/misc/yt.js
+++ plugins/misc/yt.js
@@ -4,7 +4,7 @@ let { JSDOM } = require('jsdom')
 const { Innertube, UniversalCache } = require('youtubei.js');
 const { readFileSync, existsSync, mkdirSync, createWriteStream } = require('fs');
 const {streamToIterable} = require('youtubei.js/dist/src/utils/Utils');
-const misc = require('./misc.js');
+const misc = require('./misc/misc');
 function bytesToSize(bytes) {
   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
   if (bytes == 0) return '0 Byte';
