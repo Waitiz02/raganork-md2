94bc5b8c076ee29390a79928ff23de176bef3556
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-02-26 21:05:49 +0530

    fix attp

--- plugins/converters.js
+++ plugins/converters.js
@@ -134,9 +134,7 @@ Module({
     desc: "Text to animated sticker"
 }, (async (message, match) => {
     if (match[1] == '') return await message.send("*Need text*")
-    try { var result = await skbuffer("https://api.xteam.xyz/attp?file&text="+encodeURI(match[1]))
-    return await message.sendReply(result,'sticker');
-    } catch {var result = await skbuffer("https://raganork-api.herokuapp.com/api/attp?text="+encodeURI(match[1] +"&apikey=with_love_souravkl11"))} 
+    var result = await skbuffer("https://raganork-api.herokuapp.com/api/attp?text="+encodeURI(match[1] +"&apikey=with_love_souravkl11")) 
     fs.writeFile("attp.mp4",result,async (e)=>{
         var exif = {
             author: STICKER_DATA.split(";")[1] || "",
@@ -146,5 +144,6 @@ Module({
             ios: "https://github.com/souravkl11/Raganork-md/"
         }
         await message.send(fs.readFileSync(await addExif(await sticker("attp.mp4",'video'),exif)),'sticker')
+    
     })
 }));
