53c294dc56a96ee616cf2fc1dc768ccbd8489fda
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-03-19 20:48:31 +0530

    Update tag.js

--- plugins/tag.js
+++ plugins/tag.js
@@ -37,8 +37,10 @@ Module({pattern: 'forward ?(.*)',use: 'utility', fromMe: true, desc: "Forwards m
 }));
 Module({pattern: 'send ?(.*)',use: 'utility', fromMe: true, desc: "Sends message"}, (async (message, match) => {
     if (!message.reply_message) return await message.sendReply("*Reply to a message*\n*Ex: .send jid jid ...*")
+     if (!match[1]) match[1] = message.jid
     let Jids = [...match[1]?.match(/[0-9]+(-[0-9]+|)(@g.us|@s.whatsapp.net)/g)] || [message.jid];
         for (let jid of Jids) {
-      await message.forwardMessage(jid, message.quoted,{contextInfo:{isForwarded: false},detectLinks: true});
-    }
+     await message.forwardMessage(jid, message.quoted,{contextInfo:{isForwarded: false},detectLinks: true});
+    await new Promise((r) => setTimeout(r, 5000))
+        }
 }));
