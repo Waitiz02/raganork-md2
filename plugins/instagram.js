df7c5942ca66b4ea9a3a83451f06c07e1a32430a
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-04-04 17:53:01 +0530

    fixed facebook dl (.fb)

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -76,23 +76,8 @@ Module({
      let _q = !msg.reply_message.message ? query[1] : msg.reply_message.message
      if (/\bhttps?:\/\/\S+/gi.test(q)) q = q.match(/\bhttps?:\/\/\S+/gi)[0]
      if (!q) return await msg.sendReply("*Need Facebook link*")
-     if (!_q.includes("!")){
-const buttons = [
-  {buttonId: hnd+'fb '+q+' !hd', buttonText: {displayText: 'HD'}, type: 1},
-  {buttonId: hnd+'fb '+q+' !sd', buttonText: {displayText: 'SD'}, type: 1}
- ]
-const buttonMessage = {
-    text: "*Select video quality*",
-    footer: '',
-    buttons: buttons,
-    headerType: 1
-}
- await msg.client.sendMessage(msg.jid, buttonMessage,{quoted:msg.data})
-     }
-    if (_q.includes("!")){
      var res = await fb(q);
-     return await msg.sendReply({url: res[_q.split("!")[1]]},"video")
-     }
+     return await msg.sendReply({url: res['hd']},"video")
         }));
 Module({
     pattern: 'ig ?(.*)',
