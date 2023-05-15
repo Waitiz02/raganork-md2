037c35b1951278dbf34c38f908be620dedf76614
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-05-15 21:18:27 +0530

    reply msg edit for ping

--- plugins/alive.js
+++ plugins/alive.js
@@ -133,7 +133,7 @@ Module({
   desc: 'Measures ping'
 }, (async (message, match) => {
   const start = new Date().getTime()
-  let sent_msg = await message.send('*❮ ᴛᴇsᴛɪɴɢ ᴘɪɴɢ ❯*')
+  let sent_msg = await message.sendReply('*❮ ᴛᴇsᴛɪɴɢ ᴘɪɴɢ ❯*')
   const end = new Date().getTime()
   await message.edit('*ʟᴀᴛᴇɴᴄʏ: ' + (end - start) + ' _ᴍs_*',message.jid,sent_msg.key)
 }));
