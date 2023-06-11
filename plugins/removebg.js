5b9424b4c475a4b037f01c0987b589d7dd056cba
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-06-11 17:46:00 +0530

    added 2 upscaler engines

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -7,7 +7,7 @@ const FormData = require('form-data');
 const stream = require('stream');
 const {promisify} = require('util');
 const pipeline = promisify(stream.pipeline);
-const {removeBgv2, imageUpscaler} = require('./misc/editors');
+const {removeBgv2, imageUpscaler, imageUpscalerV2} = require('./misc/editors');
 Module({pattern: 'removebg ?(.*)', fromMe: w,use: 'AI', desc: "Removes image background using AI"}, (async (message, match) => {    
 if (!message.reply_message?.image) return await message.send("_Reply to a photo_");
 let removing = await message.sendReply('_Removing background.._')
@@ -37,7 +37,14 @@ if (!RBG_KEY) {
     }));
 Module({pattern: 'upscale ?(.*)', fromMe: w,use: 'AI', desc: "Enhances/upscales image quality using AI"}, (async (message, match) => {    
 if (!message.reply_message?.image) return await message.send("_Reply to a photo_");
-let removing = await message.sendReply('_Upscaling image.._')    
-await message.client.sendMessage(message.jid,{image:await imageUpscaler(await message.reply_message.download('buffer')),caption:"_Enhanced image_"},{quoted:message.quoted})
+if (!match[1] || (match[1] != "1" && match[1] != "2")) return await message.sendReply("_Need type (1 or 2)_\n_Ex: .upscale 2_\n_(Note: 1 & 2 use different enhancing engines)_")
+let removing = await message.sendReply('_Upscaling image.._') 
+let image;
+if (match[1] == "1"){
+    image = await imageUpscaler(await message.reply_message.download('buffer'))
+    } else {
+    image = await imageUpscalerV2(await message.reply_message.download())
+}   
+await message.client.sendMessage(message.jid,{image,caption:"_Enhanced image_"},{quoted:message.quoted})
 await message.edit('_Task complete!_',message.jid,removing.key)
 }));
