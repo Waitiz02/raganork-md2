6441ced87678df6d970b86b8d3273d51adc78d48
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-06-11 18:52:53 +0530

    fixed upscale

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -37,15 +37,11 @@ if (!RBG_KEY) {
     }));
 Module({pattern: 'upscale ?(.*)', fromMe: w,use: 'AI', desc: "Enhances/upscales image quality using AI"}, (async (message, match) => {    
 if (!message.reply_message?.image) return await message.send("_Reply to a photo_");
-if (!match[1] || (match[1] != "1" && match[1] != "2")) return await message.sendReply("_Need type (1 or 2)_\n_Ex: .upscale 2_\n_(Note: 1 & 2 use different enhancing engines)_")
 let removing = await message.sendReply('_Upscaling image.._') 
-let image;
-if (match[1] == "1"){
-    image = await imageUpscaler(await message.reply_message.download('buffer'))
-    } else {
-    image = await imageUpscalerV2(await message.reply_message.download())
-}   
-if (!image) return await message.sendReply(`_Engine ${match[1]} error, please use ${match[1] =="1"?"2":"1"}_`)
+try {
+let image = await imageUpscaler(await message.reply_message.download('buffer'))
 await message.client.sendMessage(message.jid,{image,caption:"_Enhanced image_"},{quoted:message.quoted})
 await message.edit('_Task complete!_',message.jid,removing.key)
-}));
+}catch{
+return await message.sendReply("_Request failed! Image too large?_")
+}}));
