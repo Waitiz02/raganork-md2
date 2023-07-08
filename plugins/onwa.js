c785e2311101ed7dc96d0e030c957f31f509e5d6
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-07-08 16:57:00 +0530

    onwa: processing msg

--- plugins/onwa.js
+++ plugins/onwa.js
@@ -12,5 +12,7 @@ Module({
     usage: 'onwa +48888888xxx'
 }, (async (message, match) => {
     if (!match[1]) return await message.sendReply("_Need number!_");
-    return await processOnwa(message,match[1])
+    let x = await message.send("_Processing.._");
+    await processOnwa(message,match[1])
+    return await message.edit('_Task complete!_',message.jid,x.key)
 }));
