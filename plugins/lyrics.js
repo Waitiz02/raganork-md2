d49714e5778a10ae2a08b23a22070cc3b4469391
Sourav <87891508+souravkl11@users.noreply.github.com>
2023-07-26 14:24:07 +0530

    fixed lyrics

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -11,7 +11,7 @@ Module({
     use: 'download'
 }, (async (message, match) => {
     if (!match[1]) return await message.sendReply("_Need query!_");
-    let result = await getJson(`https://raganork.ml/api/lyrics?query=${encodeURIComponent(match[1])}`)
+    let result = await getJson(`https://raganork.tk/api/lyrics?query=${encodeURIComponent(match[1])}`)
     if (!result.status) return await message.sendReply('_No results found!_')
     let ch = '```'
     return await message.sendReply(`${ch}Title: "${result.title}"\n\n${result.result}${ch}`);
